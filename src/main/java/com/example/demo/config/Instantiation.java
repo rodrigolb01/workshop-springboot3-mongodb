a72b183d193a57dc84edf621f60dfef934bdc52e
Rodrigol <rodrigolb01@gmail.com>
2023-09-15 11:09:35 -0300

    instantiating the database

---
+++ src/main/java/com/example/demo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.example.demo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	UserRepository repo;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		repo.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		repo.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}

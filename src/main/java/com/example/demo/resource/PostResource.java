7d533292bb09f0004d60ee64b0a7e707a80009d0
Rodrigol <rodrigolb01@gmail.com>
2023-09-19 12:02:00 -0300

    text and date range search

--- src/main/java/com/example/demo/resource/PostResource.java
+++ src/main/java/com/example/demo/resource/PostResource.java
@@ -1,5 +1,6 @@
 package com.example.demo.resource;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,19 @@ public class PostResource {
 	{
 		title = URL.decodeParam(title);
 		List<Post> posts = service.findByTitle(title);
+		return ResponseEntity.ok().body(posts);
+	}
+	
+	@RequestMapping(value = "/fullSearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate)
+	{
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> posts = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(posts);
 	}
 }
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +25,11 @@ public class PostService {
 	public List<Post> findByTitle(String title)
 	{
 		return repo.findByTitleContainingIgnoreCase(title);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate)
+	{
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
